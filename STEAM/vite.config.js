ea736a3e619076e8bc117599df3f759fa9ebaba0
pedaeadr-hash <pedaeadr@gmail.com>
2026-01-30 11:42:44 -0300

    ajustes gitpages

--- STEAM/vite.config.js
+++ STEAM/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Steam/' //FIZ npm install gh-pages --save-dev E DEPOIS aq coloquei o nome do repo
 })
