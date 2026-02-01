fe3140829d5844b7a0780dfdb53f12b2fc85bd7b
pedaeadr-hash <pedaeadr@gmail.com>
2026-02-01 14:37:52 -0300

    loading

--- STEAM/src/pages/Detalhes/Detalhes.jsx
+++ STEAM/src/pages/Detalhes/Detalhes.jsx
@@ -48,8 +48,9 @@ function Detalhes () {
 
 
             <div className="mb">
+                <div className="bb"> 
                 <iframe src={JOGO.media} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
+                </div>
                 <h1 className="mbt">{JOGO.title}</h1>
                 <p className="mbs">{JOGO.sinops}</p>
 
