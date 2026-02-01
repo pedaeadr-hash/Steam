7c82d10ee833cba476aed6b69f4d5b3472d2105f
pedaeadr-hash <pedaeadr@gmail.com>
2026-02-01 13:25:09 -0300

    scrolltop

--- STEAM/src/App.jsx
+++ STEAM/src/App.jsx
@@ -1,10 +1,12 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Index";
 import Detalhes from "./pages/Detalhes/Detalhes";
+import Scrolltop from "./Scrolltop";
 
 function App () {
   return (
     <HashRouter>
+      <Scrolltop/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Detalhes/:slug" element={<Detalhes />} />
