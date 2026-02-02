084eaf87570e847a7659562f05dd85a0360ed998
pedaeadr-hash <pedaeadr@gmail.com>
2026-02-02 20:40:42 -0300

    ajustes

--- STEAM/src/pages/Detalhes/Detalhes.jsx
+++ STEAM/src/pages/Detalhes/Detalhes.jsx
@@ -34,7 +34,7 @@ function Detalhes () {
                         <div className="p"><h1 className="ver" onClick={chamar}>ver mais</h1><h1 className="vmb" onClick={chamar}>ver mais</h1>{JOGO.plataformaPC && <img src={JOGO.plataformaPC} alt="plataforma PC" />}{JOGO.plataformaPL && <img src={JOGO.plataformaPL} alt="plataforma Play" />}{JOGO.plataformaXB && <img src={JOGO.plataformaXB} alt="plataforma XBOX"/>}
                         </div>
                 </div>
-                <div className="popt" ref={pop}>
+                <div className="popz" ref={pop}>
                     <div className="barx" onClick={volt}><svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 16 16"><path fill="#ffffff" d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94z"/></svg></div>
                     <div className="tf"><img src={Platina} alt="trofeu de platina" /><h1>{JOGO.trofeuPL}</h1></div>
                     <div className="tf"><img src={Ouro} alt="trofeu de Ouro" /><h1>{JOGO.trofeuOR}</h1></div>
